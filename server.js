d6dd3f490e31add0f04f42a6bec318ca02142233
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-06-17 22:49:42 -0700

    added app get function

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-const express = require ("express");
+const express = require("express");
 const path = require("path");
-const fs = require ("fs");
+const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 let notes = [];
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api/notes", function(err,res){
-res.json(notes)
+app.get("/api/notes", function (err, res) {
+  try {
+    notesData = fs.readFileSync("db/db.json", "utf8");
+    
+    notesData = JSON.parse(notesData);
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.json(notesData);
 });
 
+app.post("/api/notes", function (req, res) {
+  try {
+    notesData = fs.readFileSync("./db/db.json", "utf8");
+    
+
+   
