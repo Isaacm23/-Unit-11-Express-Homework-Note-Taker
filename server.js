da247916a5333da496296f0734692fcf49df916d
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-06-17 21:03:08 -0700

    added app get ntes

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const fs = require ("fs");
 const app = express();
 const PORT = process.env.PORT || 8080
 
+let notes = [];
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/notes", function(err,res){
+res.json(notes)
+});
+
